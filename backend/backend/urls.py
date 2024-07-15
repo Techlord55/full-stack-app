bdc2601373aec84e468942ae2160ae32026b4dc7
TCHABEU  STEPHANE <tchabeustephane2@gmail.com>
2024-07-15 16:29:13 +0100

    4rd commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,10 +4,10 @@ from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/user/register/", CreateUserView.as_view(), name="register"),  # Added missing comma
-    path('api/token/', TokenObtainPairView.as_view(), name="get_token"),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("admin/", admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
 ]
